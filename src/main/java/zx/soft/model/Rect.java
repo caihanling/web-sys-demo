bab08ffa231c4ad2a04af7655dc737ecebe7a371
caihanling <caihanling@zxisl.com>
2016-04-27 16:19:49 +0800

    删除文件

---
+++ src/main/java/zx/soft/model/Rect.java
@@ -0,0 +1,58 @@
+package zx.soft.model;
+
+public class Rect {
+	private String x;
+	private String y;
+	private String width;
+	private String height;
+	private String node;  //区域编号
+	private String parentNode;		//父节�?
+	private String computerNum;		//区域包含电脑�?
+	
+	
+	public String getX() {
+		return x;
+	}
+	public void setX(String x) {
+		this.x = x;
+	}
+	public String getY() {
+		return y;
+	}
+	public void setY(String y) {
+		this.y = y;
+	}
+	public String getWidth() {
+		return width;
+	}
+	public void setWidth(String width) {
+		this.width = width;
+	}
+	public String getHeight() {
+		return height;
+	}
+	public void setHeight(String height) {
+		this.height = height;
+	}
+	public String getNode() {
+		return node;
+	}
+	public void setNode(String node) {
+		this.node = node;
+	}
+	public String getParentNode() {
+		return parentNode;
+	}
+	public void setParentNode(String parentNode) {
+		this.parentNode = parentNode;
+	}
+	public String getComputerNum() {
+		return computerNum;
+	}
+	public void setComputerNum(String computerNum) {
+		this.computerNum = computerNum;
+	}
+	
+	
+
+}
